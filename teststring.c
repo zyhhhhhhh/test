5149046555a54497735169378f0971aa4b69b04e
Yihao <yzhng127@illinois.edu>
2015-10-16 13:09:47 -0500

    aaa

--- teststring.c
+++ teststring.c
@@ -3,7 +3,7 @@
 int main()
 {
     char strings[20] = "test the input";
-    int len_of_command = sizeof(code)/(sizeof(char));
+    int len_of_command = sizeof(string)/(sizeof(char));
     printf("len = %d\n", len_of_command);
     return 0;
 }
